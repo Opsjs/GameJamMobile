d6c7fb2a71f33af33257a37480372ecc4f487250
Opsjs <106817113+Opsjs@users.noreply.github.com>
2025-01-10 10:49:24 +0100

    Add working camera (raw movement)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float newVerticalPosition = 0;
+    float oldVerticalPosition = 0;
+    float highestVerticalPosition = 0;
+    [SerializeField] Transform cameraTransform;
     private void Update()
     {
         float tilt = -Input.acceleration.x;
-        Debug.Log(tilt);
         if (tilt > 0.1f || tilt < -0.1f)
         {
             tilt = 0;
@@ -17,6 +20,22 @@ public class PlayerController : MonoBehaviour
         } else if (transform.position.x > 2.5f)
         {
             transform.position = (new Vector3(2.5f, transform.position.y, transform.position.z));
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        oldVerticalPosition = newVerticalPosition;
+        newVerticalPosition = transform.position.y;
+        if (newVerticalPosition > highestVerticalPosition)
+        {
+            highestVerticalPosition = newVerticalPosition;
+            if (highestVerticalPosition > oldVerticalPosition)
+            {
+                cameraTransform.position = new Vector3(cameraTransform.position.x, newVerticalPosition, cameraTransform.position.z);
+            }
         }
     }
 
